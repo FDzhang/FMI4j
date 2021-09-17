e7250dd4078d44560865b784a623a60643894d9e
Lars Ivar Hatledal <laht@ntnu.no>
2021-09-17 12:39:05 +0200

    update

--- fmi-import/src/test/java/no/ntnu/ihb/fmi4j/importer/ControlledTemperatureTestJava.java
+++ fmi-import/src/test/java/no/ntnu/ihb/fmi4j/importer/ControlledTemperatureTestJava.java
@@ -11,12 +11,14 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
-
+@DisabledOnOs(OS.MAC)
 public class ControlledTemperatureTestJava {
 
     private final static Logger LOG = LoggerFactory.getLogger(ControlledTemperatureTestJava.class);
